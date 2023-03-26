36c8f005909bd11415cf6ead2954e250e46f8a89
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-03-26 20:20:32 +0800

    Update index.js
    Add addTask() function

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const crypto = require("crypto");
+// function openSideNav() {
+//   document.getElementById("sideNavBar").style.width = "240px";
+// }
 
-function openSideNav() {
-  document.getElementById("sideNavBar").style.width = "240px";
-}
-
-function closeSideNav() {
-  document.getElementById("sideNavBar").style.width = "0";
-}
+// function closeSideNav() {
+//   document.getElementById("sideNavBar").style.width = "0";
+// }
 
 function openModal() {
-  var modal = document.getElementById("idModal");
-  var openButton = document.getElementsByClassName("add-button")[0];
-  var closeButton = document.getElementsByClassName("close-button")[0];
+  let modal = document.getElementById("idModal");
+  let openButton = document.getElementsByClassName("add-button")[0];
+  let closeButton = document.getElementsByClassName("close-button")[0];
 
   openButton.onclick = () => {
     modal.style.display = "block";
@@ -20,4 +18,45 @@ function openModal() {
   closeButton.onclick = () => {
     modal.style.display = "none";
   };
+}
+
+function closeModal() {
+  let modal = document.getElementById("idModal");
+  modal.style.display = "none";
+}
+
+function addTask() {
+  let taskList = document.getElementById("idTaskList").value;
+  let task = document.getElementById("idTitle").value;
+  let deadline = document.getElementById("idDueDate").value;
+  let idPriority = ["idLow", "idMedium", "idHigh", "idUrgent"];
+  for (let i of idPriority) {
+    if (document.getElementById(i).checked) {
+      var urgency = document.getElementById(i).value;
+    }
+  }
+  let notes = document.getElementById("idDescription").value;
+
+  const taskArray = JSON.parse(localStorage.getItem("whattodo")) ?? [];
+  if (taskList && task) {
+    const taskObject = {
+      id: crypto.randomUUID(),
+      list: taskList,
+      title: task,
+      dueDate: deadline,
+      priority: urgency,
+      description: notes,
+      isCompleted: false,
+      isDeleted: false,
+    };
+    taskArray.push(taskObject);
+    localStorage.setItem("whattodo", JSON.stringify(taskArray));
+
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+    taskItem.innerText = taskObject.title;
+
+    const renderElement = document.getElementById("idRenderElement");
+    renderElement.appendChild(taskItem);
+  }
 }
